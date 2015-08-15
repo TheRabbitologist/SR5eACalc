98a66183db38946bf0bc430abf994ad4d4c5c142
TheRabbitologist <albatross@nosuch.net>
2015-08-15 14:01:25 -0600

    Began work on stat storage.

---
+++ srv5r/Stats.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package srv5r;
+
+import java.util.HashMap;
+
+public class Stats {
+    protected HashMap<Stat,Integer> stat;
+    protected HashMap<Stat,Integer> mod;
+    protected double ESS;
+    public Stats() {
+        stat = new HashMap<>();
+        mod = new HashMap<>();
+        for(Stat s : Stat.values()) {
+            stat.put(s, 1);
+            mod.put(s, 0);
+        }
+        stat.put(Stat.MAG, 0);
+        stat.put(Stat.RES, 0);
+        ESS = 6;
+    }
+    public int getStat(Stat s) {
+        return Math.max(0,stat.get(s) + mod.get(s));
+    }
+    public double getESS() {
+        return ESS;
+    }
+    public Stats setStat(Stat s, int val) {
+        stat.put(s, Math.max(1,val));
+        return this;
+    }
+    public Stats setStatMod(Stat s, int val) {
+        mod.put(s, val);
+        return this;
+    }
+    public Stats setStatMod(Stat s) {
+        return setStatMod(s, 0);
+    }
+    public Stats setESS(double val) {
+        ESS = Math.max(0,val);
+        return this;
+    }
+    public int getLimitM() {
+        int base = stat.get(Stat.LOG)*2+stat.get(Stat.INT)+stat.get(Stat.WIL);
+        return (int)Math.ceil(((double)base)/3);
+    }
+    public int getLimitP() {
+        int base = stat.get(Stat.STR)*2+stat.get(Stat.BOD)+stat.get(Stat.REA);
+        return (int)Math.ceil(((double)base)/3);
+    }
+    public int getLimitS() {
+        double base = stat.get(Stat.CHA)*2+stat.get(Stat.WIL)+getESS();
+        return (int)Math.ceil(base/3);
+    }
+    public int rollInitPhys() {
+        return rollInitPhys(0);
+    }
+    public int rollInitPhys(int bonusDice) {
+        return getStat(Stat.INT)+getStat(Stat.REA)+Roller.hits(1+bonusDice);
+    }
+    public int rollInitAstr() {
+        return Math.max(0, getStat(Stat.INT)*2+Roller.hits(2));
+    }
+    public int rollInitMtrxAR() {
+        return getStat(Stat.INT)+getStat(Stat.REA)+Roller.hits(1);
+    }
+    public int rollInitMtrxVR() {
+        return rollInitMtrxVR(false);
+    }
+    public int rollInitMtrxVR(boolean hot) {
+        int dice = (hot?4:3);
+        return getStat(Stat.LOG)+getStat(Stat.INT)+Roller.hits(dice);
+    }
+}

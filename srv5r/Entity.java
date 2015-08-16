20b825acc816d5a62bbdd2cddda9fb7437fc04fe
TheRabbitologist <albatross@nosuch.net>
2015-08-16 13:24:23 -0700

    Began work on attributes storage.

---
+++ srv5r/Entity.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package srv5r;
+
+import java.util.Map;
+
+public class Entity {
+
+    public Stats stats;
+
+    public Entity() {
+        this(null);
+    }
+
+    public Entity(Map<Stat, Integer> init) {
+        stats = new Stats();
+        if (init != null) {
+            for (Stat s : init.keySet())
+                stats.setStat(s, init.get(s));
+        }
+    }
+
+    public void set(String[] s) {
+        String key = s[0].toUpperCase();
+        switch (key) {
+            case "ESS":
+                stats.setESS(Float.parseFloat(s[1]));
+                break;
+            default:
+                Stat stat = Stat.get(key);
+                if (stat != null)
+                    stats.setStat(stat, Parser.sum(s[1]));
+                else
+                    System.out.println("Error: Unknown property '" + key + "'.");
+                break;
+        }
+    }
+
+    public Object get(String str) {
+        String prop = str.toUpperCase();
+        switch (prop) {
+            case "LM":
+                return stats.getLimitM();
+            case "LP":
+                return stats.getLimitP();
+            case "LS":
+                return stats.getLimitS();
+            case "ESS":
+                return stats.getESS();
+            default:
+                Stat s = Stat.get(prop);
+                if (s != null)
+                    return stats.getStat(s);
+                return null;
+        }
+    }
+}
---
+++ srv5r/Memory.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package srv5r;
+
+public class Memory {
+    public Entity self;
+    public Memory() {
+        self = new Entity();
+    }
+}

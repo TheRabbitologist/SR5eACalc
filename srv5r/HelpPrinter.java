07c3fff93cc86fb1caf8b44d292984abedf75013
TheRabbitologist <albatross@nosuch.net>
2015-08-16 13:35:00 -0700

    Updated help messages.

--- srv5r/HelpPrinter.java
+++ srv5r/HelpPrinter.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package srv5r;
 
 public class HelpPrinter {
@@ -13,6 +7,13 @@ public class HelpPrinter {
         System.out.println("In place of single-letter placeholders, a basic sum can be used.");
         System.out.println("\tExamples: 9+5 or 8 or 4-2-1 or -8+5+4");
         System.out.println("\tOnly subtraction and addition are supported.");
+        System.out.println();
+        System.out.println("Certain attributes and such can be stored and recalled.");
+        System.out.println("These values can be used within sums and tests, ex. CHA+WIL+2");
+        System.out.println("\tAttributes: First three letters of the attribute name.");
+        System.out.println("\tSpecial attributes: Same.");
+        System.out.println("\tEssence: ESS (rounded down when used in sums).");
+        System.out.println("\tLimits: L + first letter of the limit name, ex. LS for the social limit.");
         System.out.println();
         System.out.println("raw <n>: Roll nd6, display results of each roll.");
         System.out.println("dice <n>: Roll nd6, display sum of rolls.");
@@ -26,9 +27,16 @@ public class HelpPrinter {
         System.out.println("\tatest and btest should be of form n or n[l].");
         System.out.println("attack <atk> v. <def>: Perform an attack roll sequence.");
         System.out.println("\tIf the attack hits, a damage roll will be performed.");
+        System.out.println("\tatk and def may be sums.");
         System.out.println("damage <dmg> <ap> <target_armor> <target_body>: Calculate damage done.");
         System.out.println("\t<damage>: Should be of the form nP or nS for physical or stun damage.");
+        System.out.println("\tAll other arguments may be sums.");
         System.out.println("\tOutput also indicates damage type.");
+        System.out.println("set self <attribute> <v>: Set an attribute's value.");
+        System.out.println("\tLimits cannot be set, rather they are calculated from the attributes.");
+        System.out.println("\tRemember that v can absolutely be a sum, useful for technomancers.");
+        System.out.println("get self <attribute>: Recall an attribute's value.");
+        System.out.println("\tThis includes limits.");
         System.out.println("cls: Clear the screen.");
         System.out.println("quit: Exactly what it says on the tin.");
         System.out.println();
